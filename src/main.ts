b4240621f491f80391d22d8b68c6b41c70e6baa3
davide-ventriglia <90870409+davide-ventriglia@users.noreply.github.com>
2025-07-17 11:55:44 +0200

    fix: trying to print the summary as output

--- src/main.ts
+++ src/main.ts
@@ -237,7 +237,7 @@ class TestReporter {
       core.info(`Check run HTML: ${resp.data.html_url}`)
       core.setOutput('url', resp.data.url)
       core.setOutput('url_html', resp.data.html_url)
-      core.setOutput('summary', resp.output.summary)
+      core.setOutput('summary', resp.output)
     }
 
     return results
